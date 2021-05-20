5d0a8a05b5981fd385d6e9dafeadc2255b27445a
dianabasilio <dianabasilio99@hotmail.com>
2021-05-19 22:24:42 -0500

    p

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '1a2b1f374156da765ab04e31ebc25eb782d00e06',
+    'reference' => '6245c5cc216cd99e0a923f0ca395284b5ff998b5',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,16 @@
       'aliases' => 
       array (
       ),
-      'reference' => '1a2b1f374156da765ab04e31ebc25eb782d00e06',
+      'reference' => '6245c5cc216cd99e0a923f0ca395284b5ff998b5',
+    ),
+    'symfony/apache-pack' => 
+    array (
+      'pretty_version' => 'v1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3aa5818d73ad2551281fc58a75afd9ca82622e6c',
     ),
   ),
 );
