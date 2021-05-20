68324bc8e59e3f83f1f5cbd0ea92062d53ad0375
dianabasilio <dianabasilio99@hotmail.com>
2021-05-19 20:37:07 -0500

    j

--- conexion.php
+++ conexion.php
@@ -7,6 +7,11 @@ class conexion{
     const db='sql3412912';
     const servidor='sql3.freemysqlhosting.net';
 
+    #const user='id16792821_root';
+    #const pass='bSUBSHU1PN';
+    #const db='id16792821_databaseservidoriot';
+    #const servidor='files.000webhost.com';
+
     public function conectardb(){
         $conectar = new mysqli(self::servidor, self::user,self::pass,self::db);
         if($conectar->connect_errno){
