9e69b98cef11915b3a8deddb6feffcd4634e2147
dianabasilio <dianabasilio99@hotmail.com>
2021-05-19 23:16:35 -0500

    p

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '476ff7d0b101209de9ca63e593bf10f11f41d722',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '476ff7d0b101209de9ca63e593bf10f11f41d722',
+    ),
+  ),
+);
